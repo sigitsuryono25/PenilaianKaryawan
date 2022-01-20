01efcb6d8d6071df725a5894b3db0d458c9576a4
Sigit Suryono <sigitharsy25@gmail.com>
2022-01-20 08:10:51 +0700

    change new response penilaian

--- app/src/main/java/com/surelabsid/lti/penilaiankaryawan/response/ResponsePenilaian.kt
+++ app/src/main/java/com/surelabsid/lti/penilaiankaryawan/response/ResponsePenilaian.kt
@@ -2,64 +2,71 @@ package com.surelabsid.lti.penilaiankaryawan.response
 
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
-import kotlinx.parcelize.Parcelize
+import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class ResponsePenilaian(
 
-    @field:SerializedName("code")
-    val code: Int? = null,
+	@field:SerializedName("code")
+	val code: Int? = null,
 
-    @field:SerializedName("message")
-    val message: String? = null,
+	@field:SerializedName("message")
+	val message: String? = null,
 
-    @field:SerializedName("data_penilaian")
-    val dataPenilaian: List<DataPenilaianItem?>? = null
+	@field:SerializedName("data_penilaian")
+	val dataPenilaian: List<DataPenilaianItem?>? = null
 ) : Parcelable
 
 @Parcelize
-data class PairNilaiXidPointItem(
+data class PenilaianItem(
 
-    @field:SerializedName("nilai")
-    val nilai: Double? = null,
+	@field:SerializedName("idBidang")
+	val idBidang: String? = null,
 
-    @field:SerializedName("idPoint")
-    val idPoint: String? = null
+	@field:SerializedName("bobotPoint")
+	val bobotPoint: String? = null,
+
+	@field:SerializedName("nilaiAkhirPerPoint")
+	val nilaiAkhirPerPoint: String? = null,
+
+	@field:SerializedName("nilaiDanPoint")
+	val nilaiDanPoint: List<NilaiDanPointItem?>? = null
 ) : Parcelable
 
 @Parcelize
 data class DataPenilaianItem(
 
-    @field:SerializedName("dinilai_pada")
-    val dinilaiPada: String? = null,
+	@field:SerializedName("dinilai_pada")
+	val dinilaiPada: String? = null,
 
-    @field:SerializedName("id_kar")
-    val idKar: String? = null,
+	@field:SerializedName("penilaian")
+	val penilaian: List<PenilaianItem?>? = null,
 
-    @field:SerializedName("jabatan_kar")
-    val jabatanKar: String? = null,
+	@field:SerializedName("id_kar")
+	val idKar: String? = null,
 
-    @field:SerializedName("nama_kar")
-    val namaKar: String? = null,
+	@field:SerializedName("nama_kar")
+	val namaKar: String? = null,
 
-    @field:SerializedName("id_penilai")
-    val idPenilai: String? = null,
+	@field:SerializedName("jabatan_kar")
+	val jabatanKar: String? = null,
 
-    @field:SerializedName("data_penilaian")
-    val dataPenilaian: List<DataPenilaianItem?>? = null,
+	@field:SerializedName("id_penilai")
+	val idPenilai: String? = null,
 
-    @field:SerializedName("nama_penilai")
-    val namaPenilai: String? = null,
+	@field:SerializedName("nama_penilai")
+	val namaPenilai: String? = null
+) : Parcelable
 
-    @field:SerializedName("nilaiAKhirPerPoint")
-    val nilaiAKhirPerPoint: String? = null,
+@Parcelize
+data class NilaiDanPointItem(
 
-    @field:SerializedName("bobotPoint")
-    val bobotPoint: String? = null,
+	@field:SerializedName("nilai")
+	val nilai: Double? = null,
 
-    @field:SerializedName("idBidang")
-    val idBidang: String? = null,
+	@field:SerializedName("idPoint")
+	val idPoint: String? = null,
 
-    @field:SerializedName("pairNilaiXidPoint")
-    val pairNilaiXidPoint: List<PairNilaiXidPointItem?>? = null
+	@field:SerializedName("giveScore")
+	val giveScore: Double? = null
 ) : Parcelable
